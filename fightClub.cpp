bb1833796603b90269136f57146dc2b33cf3f06a
kaiserlike <franzdietrich@gmx.at>
2016-05-01 20:14:36 +0200

    first draft

---
+++ fightClub.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include "fighter.h"
+
+using namespace std;
+
+int main(){
+
+return 0;
+}
---
+++ fighter.cpp
@@ -0,0 +1,23 @@
+#include "fighter.h"
+
+fighter::fighter(string name){      //constructor fighter
+    nickname = name;
+
+    defensePoints = rand() % 26 + 25;       // random number between 25 an 50
+    offensePoints = rand() % 26 + 25;       // random number between 25 an 50
+}
+
+warrior::warrior(string name) : fighter(name){};    //constructor warrior
+
+ninja::ninja(string name) : fighter(name){};        //constructor ninja
+
+void warrior::warriorSpecial(){
+   int rng = rand() % 6;
+   if(rng == 5){
+        hitPoints = hitPoints * 2;
+   }
+}
+
+void fighter::turn(string name){
+
+}
