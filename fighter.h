2c9b0f57e2a985d4f37c1e1f94e766f8a8b4e384
Layanasan <chidori@gmx.at>
2016-05-03 16:32:07 +0200

    Versuch 2

--- fighter.h
+++ fighter.h
@@ -8,37 +8,44 @@
 using namespace std;
 
 class fighter {
-public:
-    fighter(string name);
-    void turn(string name);
+    public:
+        fighter(string name);
+        void turn(string name);
 
-protected:
-    string nickname;
-    int lifePoints = 100;
-    int defensePoints, offensePoints, hitPoints;
+    protected:
+        string nickname;
+        int lifePoints = 100;
+        int defensePoints, offensePoints, hitPoints;
 
 };
 
 class warrior : public fighter {
-public:
-    warrior(string name);
+    public:
+        warrior(string name);
 
-private:
-    void warriorSpecial();
+    private:
+        void warriorSpecial();
 };
 
 class ninja : public fighter {
-public:
-    ninja(string name);
-private:
-    int ninjaSpecial();
+    public:
+        ninja(string name);
+    private:
+        int ninjaSpecial();
 };
 
 class link : public fighter {
-public:
-    link(string name);
-private:
-    int linkSpecial();
+    public:
+        link(string name);
+    private:
+        int linkSpecial();
+};
+
+class zombie : public fighter {
+    public:
+        zombie(string name);
+    private:
+        int unDead();
 };
 
 #endif // FIGHTER_H_INCLUDED
